8501a37087e98d80e2b44541089c41372b92be42
Javier Flores <javerflors@gmail.com>
2025-04-12 12:39:47 -0600

    fix: update MongoDB connection options to improve connection handling

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectWithRetry = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false, // Deshabilita la creación automática de índices
+    });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
